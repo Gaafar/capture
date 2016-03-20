3ddf19aaec3964ac168d4b0fd41cd31170e52e02
Mostafa Gaafar <mgaafar@live.com>
2016-03-20 09:50:26 +0400

    feat: update options for api

--- server.js
+++ server.js
@@ -14,31 +14,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 /*
-
 get screenshot of a web page
 
 params (query):
     url :: String, page url
     width :: int, page width to simulate
-    height :: int or 'all', screenshot height, default: 'all' captures whole page
-    fixed :: Bool, adds css to page to disable animations
+    height :: int, page height to simulate, does not affect screenshot height as it will capture whole page anyway
+    anim :: Bool, enable css animations
     ua :: USER_AGENTS, user agent to simulate
 */
 app.get('/api/capture', (req, res) => {
 
-    let { url, width = 320, height = 568, ua = null, fixed = false } = req.query;
+    let { url, width = 320, height = 568, ua = null, anim = false } = req.query;
 
     console.log(`/api/capture url:${url}`)
+
+    // exit if no url sent
     if (!url) return res.sendStatus(404);
 
     const USER_AGENTS = {
         "mobile": 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'
     };
-
-    const CUSTOM_CSS =
-        `body 
-            {animation: none !important; -webkit-animation: none !important;}`
-
+    const BLOCK_ANIM_CSS = `body {animation: none !important; -webkit-animation: none !important;}`
 
     let options = {
         screenSize: {
@@ -50,15 +47,15 @@ app.get('/api/capture', (req, res) => {
             height: 'all'
         },
         userAgent: USER_AGENTS[ua],
-        customCSS: fixed ? CUSTOM_CSS : null,
+        customCSS: anim ? null : BLOCK_ANIM_CSS,
 
         // uncomment this if needed to delay screen shot
-        renderDelay: 2000
+        renderDelay: 1000,
+
         phantomConfig: { 'ignore-ssl-errors': 'true' }
     };
 
     let renderStream = webshot(decodeURIComponent(url), options);
-
     renderStream.pipe(res);
 })
 
